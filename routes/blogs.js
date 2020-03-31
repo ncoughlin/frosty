5eeb59bc45624d779cbab5e5027b1569d3ea0c0b
Nick Coughlin <coughlin.nicholas@gmail.com>
2020-03-31 17:24:38 +0000

    edit blogs route and form now uses username instead of author field.

--- routes/blogs.js
+++ routes/blogs.js
@@ -45,25 +45,25 @@ router.get("/",(req, res) => {
     });
 });
 
-// new post form
+// new blog form
 router.get("/new", isLoggedIn, (req, res) => {
     res.render("newBlog.ejs");
 });
 
-// edit post form
+// edit blog form
 router.get("/:id/edit", isLoggedIn, (req, res) => {
-     // find post with provided ID
-    Blog.findById(req.params.id,(err, dbData) => {
+     // find blog with provided ID
+    Blog.findById(req.params.id,(err, foundBlog) => {
         if(err){
             console.log("error finding blog data by ID");
         } else {
-            // render single post template with that post data
-            res.render("editBlog.ejs", {blog: dbData});
+            // render single blog template with that post data
+            res.render("editBlog.ejs", {blog: foundBlog});
         }
     });
 });
 
-// render individual post. This is a wildcard link and must therefore be
+// render individual blog. This is a wildcard link and must therefore be
 // placed after static links in the application!
 router.get("/:id",(req, res) => {
     // Find Blog by ID and populate comments
@@ -127,30 +127,23 @@ router.post("/", isLoggedIn, (req, res) => {
 // .PUT routes
 //----------------------------
 
-// edit post
+// edit blog
 router.put("/:id",(req, res) => {
     // sanitize inputs
     req.body.blog.title = req.sanitize(req.body.blog.title);
     req.body.blog.short = req.sanitize(req.body.blog.short);
     req.body.blog.content = req.sanitize(req.body.blog.content);
-    // find and update post
-    Blog.findByIdAndUpdate(req.params.id, req.body.blog,(err, oldDatabaseRecord) => {
+    // find and update blog
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog,(err, oldBlog) => {
         if(err){
             console.log("Failed to update database");
         } else {
             console.log("Blog successfully updated in database.");
-            // we want to log the UPDATED data, not the old
-            Blog.findById(req.params.id, '_id image title author date short content' , { lean: true },(err, newDatabaseRecord) => {
-                if(err){
-                    console.log("Failed To Retreive Updated Record For Display");
-                } else {
-                    console.log(newDatabaseRecord);
-                }
-            });
             // redirect to updated single post page
             res.redirect("/blogs/" + req.params.id);
         }
     });
+    
 }); 
 
 
